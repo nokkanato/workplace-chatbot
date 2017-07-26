2197c34841527afabcfe0ea2e38b03a44d43cbfb
NOK <nokkanato@gmail.com>
2017-07-26 13:24:16 +0700

    y

--- app.js
+++ app.js
@@ -148,33 +148,33 @@ function sendTextMessage(recipientId, messageText) {
     console.log('-----------ACCESS_TOKEN-----------------');
     console.log(ACCESS_TOKEN);
 
-  // request({
-  //   uri: 'https://graph.facebook.com/v2.6/me/messages',
-  //   qs: { access_token: ACCESS_TOKEN },
-  //   method: 'POST',
-  //   json: messageData
-  //
-  // }, function (error, response, body) {
-  //   console.log('response', response);
-  //   console.log('body', body);
-  //   console.log('error', error);
-  //
-  //   if (!error && response.statusCode == 200) {
-  //     var recipientId = body.recipient_id;
-  //     var messageId = body.message_id;
-  //     console.log('------recipientId----------');
-  //     console.log(recipientId);
-  //     console.log("-------messageId---------");
-  //     console.log(messageId);
-  //
-  //     console.log("Successfully sent generic message with id %s to recipient %s",
-  //       messageId, recipientId);
-  //   } else {
-  //     console.error("Unable to send message.");
-  //     console.error(response);
-  //     console.error(error);
-  //   }
-  // });
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    console.log('response', response);
+    // console.log('body', body);
+    // console.log('error', error);
+
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+      console.log('------recipientId----------');
+      console.log(recipientId);
+      console.log("-------messageId---------");
+      console.log(messageId);
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
 }
 
 
