26c53a08a0b9886db1ec4127bb47151a3d222b0f
NOK <nokkanato@gmail.com>
2017-07-26 10:22:19 +0700

    constrct

--- app.js
+++ app.js
@@ -136,29 +136,29 @@ function sendTextMessage(recipientId, messageText) {
   function callSendAPI(messageData) {
     console.log('------messageData--------');
     console.log(messageData);
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      console.log('------recipientId----------');
-      console.log(recipientId);
-      console.log("-------messageId---------");
-      console.log(messageId);
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
+  // request({
+  //   uri: 'https://graph.facebook.com/v2.6/me/messages',
+  //   qs: { access_token: ACCESS_TOKEN },
+  //   method: 'POST',
+  //   json: messageData
+  //
+  // }, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     var recipientId = body.recipient_id;
+  //     var messageId = body.message_id;
+  //     console.log('------recipientId----------');
+  //     console.log(recipientId);
+  //     console.log("-------messageId---------");
+  //     console.log(messageId);
+  //
+  //     console.log("Successfully sent generic message with id %s to recipient %s",
+  //       messageId, recipientId);
+  //   } else {
+  //     console.error("Unable to send message.");
+  //     console.error(response);
+  //     console.error(error);
+  //   }
+  // });
 }
 
 
